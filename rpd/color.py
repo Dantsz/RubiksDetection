d467fad1cb346f52f426f5fc07ad2d0b3092e421
Dan Costinas <danscostinas@gmail.com>
2024-02-26 21:32:58 +0200

    Separate face preprocessing data

--- rpd/color.py
+++ rpd/color.py
@@ -4,7 +4,7 @@
 import cv2 as cv
 import numpy as np
 
-def color_average_hue(img):
+def color_average_hue(img: np.ndarray) -> float:
     '''Computes the average hue of a contour in an image.'''
     #Convert the image to HSV
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
--- rpd/metafeatures.py
+++ rpd/metafeatures.py
@@ -17,53 +17,70 @@ class FaceSquare:
     center: Tuple[int,int]
     relative_position: Tuple[float,float]
 
+class PreProcessingData:
+    """Structure-of-arrays containing data derived from a contour."""
+    centers_of_mass: List[Tuple[int,int] | None]
+    areas: List[float | None]
+    orientation: List[Tuple[np.ndarray,np.ndarray, np.ndarray] | None]
+    normals: List[np.ndarray | None]
+
+    def __init__(self, contours : List[np.ndarray]):
+        self.centers_of_mass = []
+        self.areas = []
+        self.orientation = []
+        self.normals = []
+        camera_matrix = orientation.build_camera_matrix(vp.ORIENTATION_ESTIMATED_FOV, vp.WIDTH, vp.HEIGHT)
+        for contour in contours:
+            M = cv.moments(contour)
+            if M["m00"] == 0:
+                self.centers_of_mass.append(None)
+                self.orientation.append(None)
+                self.areas.append(None)
+                self.normals.append(None)
+                continue
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            self.centers_of_mass.append(center)
+
+            (rotation_vector, translation_vector) = orientation.estimate_rectangle_contour_pose(contour, camera_matrix)
+            normal_vector = orientation.compute_rectangle_normal_vector(rotation_vector)
+            self.orientation.append((rotation_vector, translation_vector, normal_vector))
+
+            self.areas.append(cv.contourArea(contour))
+            self.normals.append(normal_vector)
+        pass
+    def __getitem__(self, index: int) -> Tuple[Tuple[int,int], float, Tuple[np.ndarray,np.ndarray, np.ndarray], np.ndarray] | None:
+        if (self.centers_of_mass is None or self.areas is None or self.orientation is None or self.normals is None):
+            return None
+        return (self.centers_of_mass[index], self.areas[index], self.orientation[index], self.normals[index])
+
 def detect_face(contours: List[np.ndarray]) -> List[List[FaceSquare]] | None:
     """Find the face of the cube in the contours list."""
     if len(contours) < 9: # We need at least 9 contours to form a cube for now
         return None
     camera_matrix = orientation.build_camera_matrix(vp.ORIENTATION_ESTIMATED_FOV, vp.WIDTH, vp.HEIGHT)
-    contours_orientations: List[Tuple[np.ndarray,np.ndarray, np.ndarray] | None]= []
-    # Find the orientation of each contour
-    # Find the center of mass of each contour
-    # Find contour area
-    centers_of_mass : List[Tuple[int,int] | None]= []
-    areas = []
-    for contour in contours:
-        M = cv.moments(contour)
-        if M["m00"] == 0:
-            centers_of_mass.append(None)
-            contours_orientations.append(None)
-            continue
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        centers_of_mass.append(center)
-
-        (rotation_vector, translation_vector) = orientation.estimate_rectangle_contour_pose(contour, camera_matrix)
-        normal_vector = orientation.compute_rectangle_normal_vector(rotation_vector)
-        contours_orientations.append((rotation_vector, translation_vector, normal_vector))
-
-        areas.append(cv.contourArea(contour))
+    contours_data = PreProcessingData(contours)
     # Check each contour and find 8 neighbors
     for i in range(len(contours)):
-        if contours_orientations[i] is None:
+        if contours_data.orientation[i] is None:
             continue
         face = [contours[i]]
         ids = [i]
-        (rotation_vector, translation_vector, normal_vector) = contours_orientations[i]
-        relative_positions = [(features.contour_basis_change(centers_of_mass[i], centers_of_mass[i], camera_matrix, rotation_vector, translation_vector))]
-        centers = [centers_of_mass[i]]
+        (rotation_vector, translation_vector, normal_vector) = contours_data.orientation[i]
+        relative_positions = [(features.contour_basis_change(contours_data.centers_of_mass[i], contours_data.centers_of_mass[i], camera_matrix, rotation_vector, translation_vector))]
+        centers = [contours_data.centers_of_mass[i]]
         #Find contour orientation
         for j in range(len(contours)):
-            if i == j or contours_orientations[j] is None:
+            if i == j or contours_data.orientation[j] is None:
                 continue
             # Find the distance between the centers of mass
-            distance = np.sqrt((centers_of_mass[i][0] - centers_of_mass[j][0])**2 + (centers_of_mass[i][1] - centers_of_mass[j][1])**2)
+            distance = np.sqrt((contours_data.centers_of_mass[i][0] - contours_data.centers_of_mass[j][0])**2 + (contours_data.centers_of_mass[i][1] - contours_data.centers_of_mass[j][1])**2)
             # If the distance is less than 1/3 of the perimeter of the contours, then they are neighbors
             if distance < (cv.arcLength(contours[i], True)/3 + cv.arcLength(contours[j], True)/3) and features.contorus_area_similarity(contours[i], contours[j], 0.1):
                 #Find position of the neighbor contour relative to the center contour
                 face.append(contours[j])
                 ids.append(j)
-                relative_positions.append(features.contour_basis_change(centers_of_mass[j], centers_of_mass[i], camera_matrix, rotation_vector, translation_vector))
-                centers.append(centers_of_mass[j])
+                relative_positions.append(features.contour_basis_change(contours_data.centers_of_mass[j], contours_data.centers_of_mass[i], camera_matrix, rotation_vector, translation_vector))
+                centers.append(contours_data.centers_of_mass[j])
         if len(face) == 9:
             # Create list of dictionaries
             # Sort into rows and columns by relative position
