c0f45b8264a613135ad98a8b4460573e58676f38
Dan Costinas <danscostinas@gmail.com>
2024-02-26 20:30:58 +0200

    Add color module

---
+++ rpd/color.py
@@ -0,0 +1,12 @@
+"""Color estimation for Rubik's Cube faces.
+"""
+
+import cv2 as cv
+import numpy as np
+
+def color_average_hue(img):
+    '''Computes the average hue of a contour in an image.'''
+    #Convert the image to HSV
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    #Compute the average hue
+    return cv.mean(hsv)[0]
