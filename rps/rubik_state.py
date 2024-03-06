8753cbde1c6bca603273c62ab6d424c23b57f82d
Dan Costinas <danscostinas@gmail.com>
2024-03-06 10:05:49 +0200

    Add rubik_state

--- rps/rubik_state.py
+++ rps/rubik_state.py
@@ -1,6 +1,6 @@
-
 from enum import Enum
 
+from rpd.metafeatures import Face
 
 class SquareColor(str, Enum):
     """Enum to represent the color of a square in a face of the cube."""
@@ -11,3 +11,16 @@ class SquareColor(str, Enum):
     GREEN = 4
     RED = 5
     ORANGE = 6
+
+class RubikStateEngine:
+    """Represents the state of a Rubik's cube.
+
+    It can be fed with the faces from the detection engine and it will keep track of the state of the cube.
+    """
+
+    def __init__(self):
+        pass
+
+    def consume_face(self, face: Face):
+        """Consumes a face from the detection engine and updates the state of the cube."""
+        pass
