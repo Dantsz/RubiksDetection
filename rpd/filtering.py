0888d22f3a3227f2282d5fedcf2b3900cf08e0a0
Dan Costinas <danscostinas@gmail.com>
2024-05-05 00:49:28 +0300

    Cleanup filtering module

--- rpd/filtering.py
+++ rpd/filtering.py
@@ -7,17 +7,17 @@ from . import viewport_properties as vp
 # THE CUBE SQUARES ARE WHITE AND CUBE LINES ARE BLACK
 # """
 
-def amax_adaptive_filter(img, gaussian_blur_kerner: int = vp.FILTER_GAUSSIAN_DEFAULT_KSIZE, morphological_kernel: int = vp.FILTER_MORPHOLOGICAL_DEFAULT_KSIZE) :
+def amax_adaptive_filter(img, gaussian_blur_kerner: int = vp.FILTER_GAUSSIAN_DEFAULT_KSIZE, morphological_kernel_size: int = vp.FILTER_MORPHOLOGICAL_DEFAULT_KSIZE) :
     gray =  np.amax(img, axis=2)
     gray = cv.convertScaleAbs(gray)
     gray = cv.GaussianBlur(gray, (gaussian_blur_kerner, gaussian_blur_kerner), 0)
     # Do adaptive thesholding
     thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, vp.FILTER_ADAPTIVE_THRESHOLD_BLOCK_SIZE, vp.FILTER_ADAPTIVE_THRESHOLD_CONSTANT)
     #Do close
-    close_kernel = np.ones((morphological_kernel,morphological_kernel), np.uint8)
+    close_kernel = np.ones((morphological_kernel_size,morphological_kernel_size), np.uint8)
     thresh = cv.morphologyEx(thresh, cv.MORPH_CLOSE, close_kernel)
     #Do opening
-    kernel = np.ones((morphological_kernel, morphological_kernel), np.uint8)
+    kernel = np.ones((morphological_kernel_size, morphological_kernel_size), np.uint8)
     thresh = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel)
     thresh = cv.erode(thresh, np.ones((vp.FILTER_MORPHOLOGICAL_DEFAULT_KSIZE, vp.FILTER_MORPHOLOGICAL_DEFAULT_KSIZE), np.uint8), iterations=1)
     return thresh
