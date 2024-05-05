e41963a666cdd2a8858106336f81f08068427af5
Dan Costinas <danscostinas@gmail.com>
2024-05-05 17:40:01 +0300

    Replace solve with solve_better implementation

--- rpd/solve.py
+++ rpd/solve.py
@@ -4,13 +4,6 @@ from . import cube_state
 from . import color
 import kociemba
 from typing import TypeAlias
-
-def solve(state: cube_state.CubeState) -> list[str]:
-    """Solves the state of the cube using the Kociemba solver."""
-    solve_str = kociemba.solve(state.to_solver_string())
-    solve_steps = [ color.spatial_symbol_to_color(move).name[0] + move[1:] for move in solve_str.split(" ")]
-    return solve_steps
-
 class MoveDirection(IntEnum):
     CLOCKWISE = 1
     COUNTERCLOCKWISE = -1
@@ -18,7 +11,7 @@ class MoveDirection(IntEnum):
 
 Move: TypeAlias = tuple[color.SquareColor, MoveDirection]
 
-def solve_but_better(state: cube_state.CubeState) -> list[Move]:
+def solve(state: cube_state.CubeState) -> list[Move]:
     """Solves the state of the cube using the Kociemba solver."""
     def __direction_str_to_int(direction: str) -> int:
         if direction == None or len(direction) == 0:
